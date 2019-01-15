08e27a24016279579d192d6a5ba7d8e816484014
Daniel Hopler <hopler.daniel@gmail.com>
2019-01-15 15:06:22 -0500

    Initialize kele with username and password with auth token

---
+++ lib/kele.rb
@@ -0,0 +1,16 @@
+require 'httparty'
+
+class Kele
+	include HTTParty
+
+	base_uri 'https://www.bloc.io/api/v1'
+
+	def initialize(username, password)
+		post_response = self.class.post('/sessions', body: {username: username, password: password})
+		if post_response.code == 404
+			puts "#{username} not found!"
+		else
+			@auth_token = post_response["auth_token"]
+		end
+	end
+end
