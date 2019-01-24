48fc45ceee0db85b4cd9abd17a624cfd31399134
Daniel Hopler <hopler.daniel@gmail.com>
2019-01-24 11:47:05 -0500

    Add remaining_checkpoints method

--- lib/kele.rb
+++ lib/kele.rb
@@ -46,4 +46,10 @@ class Kele
 		end
 	end
 
+	def get_remaining_checkpoints(chain_id)
+		response = self.class.get("/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section", headers: { "authorization" => @auth_token } )
+		JSON.parse response.body
+
+	end
+
 end
